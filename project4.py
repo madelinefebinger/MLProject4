ebbb6ac27d75074d2de4a62a17cf58052b3da70a
Madeline Febinger <febingm1@tcnj.edu>
2019-05-06 18:12:10 -0400

    Fixed the data being saved

--- project4.py
+++ project4.py
@@ -1,5 +1,6 @@
 import csv
 import perceptron
+import matplotlib.pyplot as pyplot
 
 training_examples = []
 
@@ -11,18 +12,22 @@ with open('iris.data') as csvfile:
 
 # Task 2 
 initial_weights = [0,0,0,0,0]
-
 print("LP1: Iris-setosa")
 p1 = perceptron.Perceptron(training_examples,initial_weights,"Iris-setosa")
 print(p1.results)
+print(p1.error_history)
+
 
 """
-The error is never reaching 0 on this one so I commented it out for now.
+initial_weights = [0,0,0,0,0]
+#The error is never reaching 0 on this one so I commented it out for now.
 print("LP2: Iris-versicolor")
 p2 = perceptron.Perceptron(training_examples,initial_weights,"Iris-versicolor")
 print(p2.results)
 """
 
+initial_weights = [0,0,0,0,0]
 print("LP3: Iris-virginia")
 p3 = perceptron.Perceptron(training_examples,initial_weights,"Iris-virginia")
 print(p3.results)
+print(p3.error_history)
