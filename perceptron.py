585ffe67811a1eaafc9283f4e3dea4a93f51cd5e
Madeline Febinger <febingm1@tcnj.edu>
2019-05-05 22:21:23 -0400

    Implemented perceptron learning algorithm

---
+++ perceptron.py
@@ -0,0 +1,80 @@
+import random
+
+class Perceptron: 
+	def __init__(self,training_examples,weights,target_concept):
+		self.training_examples = training_examples
+		self.weights = weights
+		self.target_concept = target_concept
+
+		self.results = []
+
+		self.perceptron_learning_alg(training_examples,weights)
+
+		#num_weights = len(training_examples[0]) - 1
+		#self.weights = self.get_random_starting_weights(num_weights)
+
+	# Perceptron learning algorithm
+	def perceptron_learning_alg(self, training_examples,weights):
+		learning_rate = 0.1
+		while (self.get_num_errors(training_examples,weights) > 0): # TODO: check if error stops decreasing
+			# Iteratively apply the perceptron to each training ex. 
+			for example in training_examples:
+				# Modifty weights 
+				delta_w_i = []
+				for i in range(len(weights)): 
+					x_i = float(example[i])
+					w_i = weights[i]
+					delta_w_i.append(learning_rate * (self.get_target_output(example) - self.output(weights,example)) * x_i)
+				for i in range(len(weights)):
+					weights[i] += delta_w_i[i]
+			self.results.append([self.get_num_errors(training_examples,weights),weights]) # Save results for this epoch 
+		print weights
+
+
+	# Returns output of the perceptron unit
+	def output(self,weights,example):
+		sum = 0
+
+		for i in range(len(weights)):
+			x = float(example[i])
+			w = weights[i]
+			sum += w*x
+
+		if (sum > 0):
+			return 1
+		else:
+			return -1
+
+
+	# Create a random starting weight vector for the perceptron
+	def get_random_starting_weights(self,num_weights): 
+		weights = []
+
+		for i in range(num_weights):
+			weight = random.uniform(-1,1)
+			weights.append(weight)
+
+		return weights
+
+	# Returns the number of errors with the given weights
+	def get_num_errors(self,training_examples,weights):
+		num_errors = 0
+
+		for example in training_examples:
+			output = self.output(weights, example)
+			target_output = self.get_target_output(example)
+
+			if (output != target_output):
+				num_errors = num_errors + 1
+
+		print(num_errors)
+		return num_errors
+
+	# Returns the target output for the given example
+	def get_target_output(self, example):
+		if (example[-1] == self.target_concept):
+			return 1
+		else:
+			return -1
+
+		
---
+++ project4.py
@@ -0,0 +1,28 @@
+import csv
+import perceptron
+
+training_examples = []
+
+with open('iris.data') as csvfile:
+	csv_reader = csv.reader(csvfile, delimiter=',')
+	for row in csv_reader:
+		row.insert(0,1) # add constant x_0 = 1 
+		training_examples.append(row)
+
+# Task 2 
+initial_weights = [0,0,0,0,0]
+
+print("LP1: Iris-setosa")
+p1 = perceptron.Perceptron(training_examples,initial_weights,"Iris-setosa")
+print(p1.results)
+
+"""
+The error is never reaching 0 on this one so I commented it out for now.
+print("LP2: Iris-versicolor")
+p2 = perceptron.Perceptron(training_examples,initial_weights,"Iris-versicolor")
+print(p2.results)
+"""
+
+print("LP3: Iris-virginia")
+p3 = perceptron.Perceptron(training_examples,initial_weights,"Iris-virginia")
+print(p3.results)
