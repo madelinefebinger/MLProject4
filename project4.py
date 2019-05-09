a4d7fc79bb752bdb9127c30fa4bbd0f9313bf424
Madeline Febinger <febingm1@tcnj.edu>
2019-05-09 18:04:33 -0400

    Created epoch stats files

--- project4.py
+++ project4.py
@@ -34,6 +34,24 @@ def get_random_starting_weights():
 
 	return weights
 
+def create_epoch_stats_file(perceptron,output_filename):
+	f = open(output_filename, "w")
+	results = perceptron.results
+	error_history = perceptron.error_history
+
+	for i in range(len(results)):
+		epoch = str(i)
+		f.write(epoch)
+		f.write('	')
+		error = str(error_history[i])
+		f.write(error)
+		f.write('	')
+		weights = str(results[i])
+		f.write(weights)
+		f.write('\n')
+
+	f.close()
+
 # Task 2, Initial Weights all 0 
 initial_weights = [0,0,0,0,0]
 print("T2 LP1: Iris-setosa")
@@ -41,33 +59,38 @@ task2_p1 = perceptron.Perceptron(training_examples,initial_weights,"Iris-setosa"
 #print(task2_p1.results)
 #print(task2_p1.error_history)
 graph(task2_p1.error_history,"T2 LP1: Iris-setosa","iris-setosa-t2.png")
+create_epoch_stats_file(task2_p1,"epoch_stats_task2_lp1.txt")
 
 initial_weights = [0,0,0,0,0]
-print("LP2: Iris-versicolor")
+print("T2 LP2: Iris-versicolor")
 task2_p2 = perceptron.Perceptron(training_examples,initial_weights,"Iris-versicolor")
 graph(task2_p2.error_history,"T2 LP2: Iris-versicolor","iris-versicolor-t2.png")
+create_epoch_stats_file(task2_p2,"epoch_stats_task2_lp2.txt")
 
 initial_weights = [0,0,0,0,0]
-print("T2 LP3: Iris-virginia")
+print("T2 LP3: Iris-virginica")
 task2_p3 = perceptron.Perceptron(training_examples,initial_weights,"Iris-virginica")
-graph(task2_p3.error_history,"T2 LP3: Iris-virginia","iris-virginica-t31.png")
+graph(task2_p3.error_history,"T2 LP3: Iris-virginica","iris-virginica-t2.png")
+create_epoch_stats_file(task2_p3,"epoch_stats_task2_lp3.txt")
 
 # Task 3.1, Initialize Weights all 1
 initial_weights = [1,1,1,1,1]
 print("T3.1 LP1: Iris-setosa")
 task31_p1 = perceptron.Perceptron(training_examples,initial_weights,"Iris-setosa")
 graph(task31_p1.error_history,"T3.1 LP1: Iris-setosa","iris-setosa-t31.png")
+create_epoch_stats_file(task31_p1,"epoch_stats_task31_lp1.txt")
 
 initial_weights = [1,1,1,1,1]
 print("LP2: Iris-versicolor")
 task31_p2 = perceptron.Perceptron(training_examples,initial_weights,"Iris-versicolor")
 graph(task31_p2.error_history,"T3.1 LP2: Iris-versicolor","iris-versicolor-t31.png")
-
+create_epoch_stats_file(task2_p1,"epoch_stats_task31_lp2.txt")
 
 initial_weights = [1,1,1,1,1]
 print("T3.1 LP3: Iris-virginica")
 task31_p3 = perceptron.Perceptron(training_examples,initial_weights,"Iris-virginica")
 graph(task31_p3.error_history,"T3.1 LP3: Iris-virginica","iris-virginica-t31.png")
+create_epoch_stats_file(task31_p3,"epoch_stats_task31_lp3.txt")
 
 # Task 3.2 Initialize weights to 4 different numbers between 0 and 1
 random_weights = get_random_starting_weights()
@@ -76,17 +99,19 @@ initial_weights = random_weights
 print("T3.2 LP1: Iris-setosa")
 task32_p1 = perceptron.Perceptron(training_examples,initial_weights,"Iris-setosa")
 graph(task32_p1.error_history,"T3.2 LP1: Iris-setosa","iris-setosa-t32.png")
+create_epoch_stats_file(task32_p1,"epoch_stats_task32_lp1.txt")
 
 initial_weights = random_weights
 print("T3.2 LP2: Iris-versicolor")
 task32_p2 = perceptron.Perceptron(training_examples,initial_weights,"Iris-versicolor")
 graph(task32_p2.error_history,"T3.2 LP2: Iris-versicolor","iris-versicolor-t32.png")
-
+create_epoch_stats_file(task32_p2,"epoch_stats_task32_lp2.txt")
 
 initial_weights = random_weights
 print("T3.2 LP3: Iris-virginica")
 task32_p3 = perceptron.Perceptron(training_examples,initial_weights,"Iris-virginica")
 graph(task32_p3.error_history,"T3.2 LP3: Iris-virginica","iris-virginica-t32.png")
+create_epoch_stats_file(task32_p3,"epoch_stats_task32_lp3.txt")
 
 # Task 3.3 Intialize weights to 4 different numbers between 
 random_weights = get_random_starting_weights()
@@ -95,17 +120,19 @@ initial_weights = random_weights
 print("T3.3 LP1: Iris-setosa")
 task33_p1 = perceptron.Perceptron(training_examples,initial_weights,"Iris-setosa")
 graph(task32_p1.error_history,"T3.3 LP1: Iris-setosa","iris-setosa-t33.png")
+create_epoch_stats_file(task33_p1,"epoch_stats_task33_lp1.txt")
 
 initial_weights = random_weights
 print("T3.3LP2: Iris-versicolor")
 task33_p2 = perceptron.Perceptron(training_examples,initial_weights,"Iris-versicolor")
 graph(task32_p2.error_history,"T3.3 LP2: Iris-versicolor","iris-versicolor-t33.png")
-
+create_epoch_stats_file(task33_p2,"epoch_stats_task33_lp2.txt")
 
 initial_weights = random_weights
 print("T3.3 LP3: Iris-virginica")
 task33_p3 = perceptron.Perceptron(training_examples,initial_weights,"Iris-virginica")
 graph(task32_p3.error_history,"T3.3 LP3: Iris-virginica","iris-virginica-t33.png")
+create_epoch_stats_file(task33_p3,"epoch_stats_task33_lp3.txt")
 
 # Task 4, Shuffle training data
 random.shuffle(training_examples)
@@ -115,16 +142,19 @@ initial_weights = [0,0,0,0,0]
 print("T4.1 LP1: Iris-setosa")
 task41_p1 = perceptron.Perceptron(training_examples,initial_weights,"Iris-setosa")
 graph(task41_p1.error_history,"T4.1 LP1: Iris-setosa","iris-setosa-t41.png")
+create_epoch_stats_file(task41_p1,"epoch_stats_task41_lp1.txt")
 
 initial_weights = [0,0,0,0,0]
 print("T4.1 LP2: Iris-versicolor")
 task41_p2 = perceptron.Perceptron(training_examples,initial_weights,"Iris-versicolor")
 graph(task41_p2.error_history,"T4.1 LP2: Iris-versicolor","iris-versicolor-t41.png")
+create_epoch_stats_file(task41_p2,"epoch_stats_task41_lp2.txt")
 
 initial_weights = [0,0,0,0,0]
-print("T4.1 LP3: Iris-virginia")
+print("T4.1 LP3: Iris-virginica")
 task41_p3 = perceptron.Perceptron(training_examples,initial_weights,"Iris-virginica")
-graph(task41_p3.error_history,"T4.1 LP3: Iris-virginia","iris-virginica-t41.png")
+graph(task41_p3.error_history,"T4.1 LP3: Iris-virginica","iris-virginica-t41.png")
+create_epoch_stats_file(task41_p3,"epoch_stats_task41_lp3.txt")
 
 # Task 4.2 Repeat Task 2 with a different shuffling order
 random.shuffle(training_examples)
@@ -133,13 +163,16 @@ initial_weights = [0,0,0,0,0]
 print("T4.2 LP1: Iris-setosa")
 task42_p1 = perceptron.Perceptron(training_examples,initial_weights,"Iris-setosa")
 graph(task42_p1.error_history,"T4.2 LP1: Iris-setosa","iris-setosa-t42.png")
+create_epoch_stats_file(task42_p1,"epoch_stats_task42_lp1.txt")
 
 initial_weights = [0,0,0,0,0]
 print("T4.2 LP2: Iris-versicolor")
 task42_p2 = perceptron.Perceptron(training_examples,initial_weights,"Iris-versicolor")
 graph(task42_p2.error_history,"T4.2 LP2: Iris-versicolor","iris-versicolor-t42.png")
+create_epoch_stats_file(task42_p2,"epoch_stats_task42_lp2.txt")
 
 initial_weights = [0,0,0,0,0]
-print("T4.2 LP3: Iris-virginia")
+print("T4.2 LP3: Iris-virginica")
 task42_p3 = perceptron.Perceptron(training_examples,initial_weights,"Iris-virginica")
-graph(task42_p3.error_history,"T4.1 LP3: Iris-virginia","iris-virginica-t42.png")
+graph(task42_p3.error_history,"T4.1 LP3: Iris-virginica","iris-virginica-t42.png")
+create_epoch_stats_file(task42_p3,"epoch_stats_task42_lp3.txt")
